df3e7d2fda0ad52822d28805e55558858c9c5bab
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2024-05-20 02:16:00 -0400

    Remove examples.py and merge to main

--- examples.py
+++
@@ -1,9 +0,0 @@
-from encryption import *
-
-publicKey = [292547, 32807]
-privateKey = [292547, 13163]
-
-print(decrypt(encrypt('Hello, world', publicKey), privateKey))
-print(keyCheck(publicKey, privateKey))
-
-print(keyBruteforce(13000, 5, publicKey))
